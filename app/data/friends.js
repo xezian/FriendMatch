bb3452175874d7e4787e42ab4941c9b7be7c1add
xezian <xezian@icloud.com>
2018-02-07 13:45:20 -0700

    closed some parentheses in display and changed the image link on one of the two stock friends so they are different

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 module.exports = [
     {
         friendName: "Beehoo",
-        pictureLink: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Mafalda_paseohist.jpg",
-        answers: [2,2,6,10,4,3,5,2,3,8]
+        pictureLink: "https://images.pexels.com/photos/39615/bearded-dragon-head-orange-reptile-39615.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+        answers: [9,8,6,10,4,3,6,7,3,8]
     },
     {
         friendName: "Boohee",
